30f4b2a64265c4f0ecc172af51be40bb8a1a574b
Guy Shaviv <guy@tivstudio.com>
2017-11-04 13:54:27 +0200

    Initial Commit

---
+++ ZCode/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ZCode
+//
+//  Created by Guy on 04/11/2017.
+//  Copyright © 2017 Houzz. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ZCode/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ZCode
+//
+//  Created by Guy on 04/11/2017.
+//  Copyright © 2017 Houzz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
